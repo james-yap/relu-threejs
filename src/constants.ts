a89e5c2b452a52ad1d1ff14c15cafcea2d07d1c2
James Y. <contact@jamesyap.org>
2026-05-31 15:30:58 -0400

    debug mode

---
+++ src/constants.ts
@@ -0,0 +1,50 @@
+export const DEBUG = true;
+
+// ref for lumens: http://www.power-sure.com/lumens.htm
+export const BULB_LUMINOUS_POWERS = {
+  '110000 lm (1000W)': 110000,
+  '3500 lm (300W)': 3500,
+  '1700 lm (100W)': 1700,
+  '800 lm (60W)': 800,
+  '400 lm (40W)': 400,
+  '180 lm (25W)': 180,
+  '20 lm (4W)': 20,
+  'Off': 0
+} as const;
+
+export type BulbPower = keyof typeof BULB_LUMINOUS_POWERS;
+export const BULB_POWER_OPTIONS = Object.keys(BULB_LUMINOUS_POWERS) as BulbPower[];
+
+// ref for solar irradiances: https://en.wikipedia.org/wiki/Lux
+export const HEMI_LUMINOUS_IRRADIANCES = {
+  '0.0001 lx (Moonless Night)': 0.0001,
+  '0.002 lx (Night Airglow)': 0.002,
+  '0.5 lx (Full Moon)': 0.5,
+  '3.4 lx (City Twilight)': 3.4,
+  '50 lx (Living Room)': 50,
+  '100 lx (Very Overcast)': 100,
+  '350 lx (Office Room)': 350,
+  '400 lx (Sunrise/Sunset)': 400,
+  '1000 lx (Overcast)': 1000,
+  '18000 lx (Daylight)': 18000,
+  '50000 lx (Direct Sun)': 50000
+} as const;
+
+export type HemiIrradiance = keyof typeof HEMI_LUMINOUS_IRRADIANCES;
+export const HEMI_IRRADIANCE_OPTIONS = Object.keys(HEMI_LUMINOUS_IRRADIANCES) as HemiIrradiance[];
+
+export type DebugParams = {
+  shadows: boolean;
+  exposure: number;
+  bulbPower: BulbPower;
+  hemiIrradiance: HemiIrradiance;
+  bulbDist: number;
+};
+
+export const DEFAULT_DEBUG_PARAMS: DebugParams = {
+  shadows: true,
+  exposure: 0.68,
+  bulbPower: BULB_POWER_OPTIONS[4],
+  hemiIrradiance: HEMI_IRRADIANCE_OPTIONS[0],
+  bulbDist: 1
+};
--- src/debug.ts
+++ src/debug.ts
@@ -1,108 +1,144 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+import {
+  BULB_LUMINOUS_POWERS,
+  BULB_POWER_OPTIONS,
+  DEBUG,
+  DEFAULT_DEBUG_PARAMS,
+  HEMI_IRRADIANCE_OPTIONS,
+  HEMI_LUMINOUS_IRRADIANCES,
+} from './constants';
+import type { DebugParams } from './constants';
 import { camera, renderer, scene } from './main';
 
+const params: DebugParams = { ...DEFAULT_DEBUG_PARAMS };
+
+
+const raycaster = new THREE.Raycaster();
+const pointer = new THREE.Vector2();
+const pointerOnPlane = new THREE.Vector3();
+const cameraWorldPos = new THREE.Vector3();
+const pointerToCamera = new THREE.Vector3();
+const bulbPos = new THREE.Vector3();
+
+const floorMat = new THREE.MeshStandardMaterial({
+  roughness: 0.8,
+  color: 0xffffff,
+  metalness: 0.2,
+  bumpScale: 1
+});
+const textureLoader = new THREE.TextureLoader();
+textureLoader.load('textures/hardwood2_diffuse.jpg', function (map) {
+
+  map.wrapS = THREE.RepeatWrapping;
+  map.wrapT = THREE.RepeatWrapping;
+  map.anisotropy = 4;
+  map.repeat.set(10, 24);
+  map.colorSpace = THREE.SRGBColorSpace;
+  floorMat.map = map;
+  floorMat.needsUpdate = true;
+
+});
+textureLoader.load('textures/hardwood2_bump.jpg', function (map) {
+
+  map.wrapS = THREE.RepeatWrapping;
+  map.wrapT = THREE.RepeatWrapping;
+  map.anisotropy = 4;
+  map.repeat.set(10, 24);
+  floorMat.bumpMap = map;
+  floorMat.needsUpdate = true;
+
+});
+textureLoader.load('textures/hardwood2_roughness.jpg', function (map) {
+
+  map.wrapS = THREE.RepeatWrapping;
+  map.wrapT = THREE.RepeatWrapping;
+  map.anisotropy = 4;
+  map.repeat.set(10, 24);
+  floorMat.roughnessMap = map;
+  floorMat.needsUpdate = true;
+
+});
+
+const floorGeometry = new THREE.PlaneGeometry(20, 20);
+const floorMesh = new THREE.Mesh(floorGeometry, floorMat);
+floorMesh.receiveShadow = true;
+// floorMesh.rotation.x = - Math.PI / 2.0;
+
+
+const bulbGeometry = new THREE.SphereGeometry(0.02, 16, 8);
+const bulbLight = new THREE.PointLight(0xffee88, 1, 100, 2);
+
+const bulbMat = new THREE.MeshStandardMaterial({
+  emissive: 0xffffee,
+  emissiveIntensity: 1,
+  color: 0x000000
+});
+bulbLight.add(new THREE.Mesh(bulbGeometry, bulbMat));
+bulbLight.position.set(0, 2, 0);
+bulbLight.castShadow = true;
+
+
+const hemiLight = new THREE.HemisphereLight(0xddeeff, 0x0f0e0d, 0.02);
+
 export function initDebug() {
+  if (!DEBUG) return;
+
+  const gui = new GUI();
+  gui.add(params, 'hemiIrradiance', HEMI_IRRADIANCE_OPTIONS);
+  gui.add(params, 'bulbPower', BULB_POWER_OPTIONS);
+  gui.add(params, 'exposure', 0, 1);
+  gui.add(params, 'shadows');
+  gui.add(params, 'bulbDist', 0, 10);
+  gui.open();
+
   const controls = new OrbitControls(camera, renderer.domElement);
   controls.minDistance = 1;
   controls.maxDistance = 20;
 
-  const cameraPositionPanel = document.getElementById('debug-panel');
-  if (cameraPositionPanel) {
-    updateCameraPositionPanel(cameraPositionPanel);
-  }
-
-  const bulbGeometry = new THREE.SphereGeometry(0.02, 16, 8);
-  const bulbLight = new THREE.PointLight(0xffee88, 1, 100, 2);
-
-  const bulbMat = new THREE.MeshStandardMaterial({
-    emissive: 0xffffee,
-    emissiveIntensity: 1,
-    color: 0x000000
-  });
-  bulbLight.add(new THREE.Mesh(bulbGeometry, bulbMat));
-  bulbLight.position.set(0, 2, 0);
-  bulbLight.castShadow = true;
   scene.add(bulbLight);
-
-  const hemiLight = new THREE.HemisphereLight(0xddeeff, 0x0f0e0d, 0.02);
   scene.add(hemiLight);
-
-  const floorMat = new THREE.MeshStandardMaterial({
-    roughness: 0.8,
-    color: 0xffffff,
-    metalness: 0.2,
-    bumpScale: 1
-  });
-  const textureLoader = new THREE.TextureLoader();
-  textureLoader.load('textures/hardwood2_diffuse.jpg', function (map) {
-
-    map.wrapS = THREE.RepeatWrapping;
-    map.wrapT = THREE.RepeatWrapping;
-    map.anisotropy = 4;
-    map.repeat.set(10, 24);
-    map.colorSpace = THREE.SRGBColorSpace;
-    floorMat.map = map;
-    floorMat.needsUpdate = true;
-
-  });
-  textureLoader.load('textures/hardwood2_bump.jpg', function (map) {
-
-    map.wrapS = THREE.RepeatWrapping;
-    map.wrapT = THREE.RepeatWrapping;
-    map.anisotropy = 4;
-    map.repeat.set(10, 24);
-    floorMat.bumpMap = map;
-    floorMat.needsUpdate = true;
-
-  });
-  textureLoader.load('textures/hardwood2_roughness.jpg', function (map) {
-
-    map.wrapS = THREE.RepeatWrapping;
-    map.wrapT = THREE.RepeatWrapping;
-    map.anisotropy = 4;
-    map.repeat.set(10, 24);
-    floorMat.roughnessMap = map;
-    floorMat.needsUpdate = true;
-
-  });
-
-  const floorGeometry = new THREE.PlaneGeometry(20, 20);
-  const floorMesh = new THREE.Mesh(floorGeometry, floorMat);
-  floorMesh.receiveShadow = true;
-  floorMesh.rotation.x = - Math.PI / 2.0;
   scene.add(floorMesh);
+
+  window.addEventListener('mousemove', (event) => {
+    // Normalize mouse coordinates from -1 to +1
+    pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+    pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+    raycaster.setFromCamera(pointer, camera);
+
+    const hits = raycaster.intersectObject(floorMesh);
+
+    if (hits.length > 0) pointerOnPlane.copy(hits[0].point)
+    pointerToCamera.subVectors(cameraWorldPos, pointerOnPlane).normalize().multiplyScalar(params.bulbDist);
+    bulbPos.addVectors(pointerOnPlane, pointerToCamera)
+    bulbLight.position.copy(bulbPos)
+  });
 }
 
-function updateCameraPositionPanel(panel: HTMLElement) {
-  const { x, y, z } = camera.position;
-  panel.textContent = `Camera position\nx: ${x.toFixed(2)}\ny: ${y.toFixed(2)}\nz: ${z.toFixed(2)}`;
+export function renderDebug() {
+  if (!DEBUG) return;
 
-  requestAnimationFrame(() => updateCameraPositionPanel(panel));
+  updateCameraPositionPanel();
+
+  bulbLight.power = BULB_LUMINOUS_POWERS[params.bulbPower];
+  bulbMat.emissiveIntensity = bulbLight.intensity / Math.pow(0.02, 2.0); // convert from intensity to irradiance at bulb surface
+  hemiLight.intensity = HEMI_LUMINOUS_IRRADIANCES[params.hemiIrradiance];
 }
 
+function updateCameraPositionPanel() {
+  camera.getWorldPosition(cameraWorldPos);
 
-// const raycaster = new THREE.Raycaster();
-// const mouse = new THREE.Vector2();
-//
-// // 2. Create a mathematical plane at Z=0 to click against
-// // (Change the normal vector if you are working on a different axis)
-// const plane = new THREE.Plane(new THREE.Vector3(0, 0, 1), 0);
-// const clickTarget = new THREE.Vector3();
-//
-// // 3. Listen for clicks
-// window.addEventListener('click', (event) => {
-//     // Normalize mouse coordinates from -1 to +1
-//     mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-//     mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-//
-//     // Cast the ray from the camera
-//     raycaster.setFromCamera(mouse, camera);
-//
-//     // Find where the ray intersects the Z=0 plane
-//     raycaster.ray.intersectPlane(plane, clickTarget);
-//
-//     if (clickTarget) {
-//         console.log(`%c📍 Clicked Coords: x: ${clickTarget.x.toFixed(2)}, y: ${clickTarget.y.toFixed(2)}, z: ${clickTarget.z.toFixed(2)}`, 'color: #00ff00; font-weight: bold;');
-//     }
-// });
+  const panel = document.getElementById('debug-panel')!
+  const { x, y, z } = cameraWorldPos;
+  panel.textContent = `Camera position
+x: ${x.toFixed(2)}
+y: ${y.toFixed(2)}
+z: ${z.toFixed(2)}
+
+Mouse position
+x: ${pointerOnPlane.x.toFixed(2)}
+y: ${pointerOnPlane.y.toFixed(2)}
+z: ${pointerOnPlane.z.toFixed(2)}`;
+}
