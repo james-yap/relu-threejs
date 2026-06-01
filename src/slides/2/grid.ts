21d5cd30532860200bd3ad22d463d05741c587b3
James Y. <contact@jamesyap.org>
2026-05-31 22:10:19 -0400

    added python jupyter notebook

--- src/slides/2/grid.ts
+++ src/slides/2/grid.ts
@@ -2,8 +2,8 @@ import * as THREE from 'three';
 
 export const slide2Group = new THREE.Group();
 
-const pos: [number, number, number] = [3.21, 1.31, 0.00]
+const pos: [number, number, number] = [3.21, 1.00, 0.00]
 
-const axesHelper = new THREE.AxesHelper(2);
+const axesHelper = new THREE.AxesHelper(3);
 slide2Group.add(axesHelper)
 slide2Group.position.set(...pos)
